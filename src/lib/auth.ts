8bd6d02f7d8aa8220bdfe1a3fa37052e7216297f
Ibrahim Balta <ibrahim@example.com>
2026-02-18 10:28:34 +0300

    fix: Vercel 494 REQUEST_HEADER_TOO_LARGE hatası için çerez yönetimi optimize edildi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -68,4 +68,32 @@ export const authOptions: NextAuthOptions = {
         signIn: "/login",
     },
     secret: process.env.NEXTAUTH_SECRET,
+    cookies: {
+        sessionToken: {
+            name: `next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === "production"
+            }
+        },
+        callbackUrl: {
+            name: `next-auth.callback-url`,
+            options: {
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === "production"
+            }
+        },
+        csrfToken: {
+            name: `next-auth.csrf-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: process.env.NODE_ENV === "production"
+            }
+        },
+    }
 }
